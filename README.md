c9a19cba33c03dc9dcc58e9cd309c7245e367e8a
AoiHosizora <a970335605@hotmail.com>
2020-01-06 17:16:29 +0800

    Update PATH & Add neo-go.cmd

--- README.md
+++ README.md
@@ -18,7 +18,13 @@
 	+ `gitgraph.bat`
 	    + `git log --graph --decorate --date=format:"%Y-%m-%d %H:%M:%S" --pretty=format:"%C(yellow)%h%C(auto)%d %C(white)%s %C(cyan)[%an] %C(red)<%cd>"`
 	+ `gitfix.bat`
-	
+
++ `bat`
+	+ `RestartEx.bat`
+	+ `neo-go.cmd`
+		+ [How to install pkg config in windows?](https://stackoverflow.com/questions/1710922/how-to-install-pkg-config-in-windows)
+		+ [mingw64-x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z](https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/8.1.0/threads-posix/seh/)
+
 + `font`
 	+ `NotoSansCJK-DemiLight` (ref)
 	+ `noto_consolas` (new)
---
+++ bat/neo-go.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+
+@set INCLUDE=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\x86_64-w64-mingw32\include;%INCLUDE%
+@set INCLUDE=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\include;%INCLUDE%
+
+
+@set LIB=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\x86_64-w64-mingw32\lib;%LIB%
+@set LIB=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\lib;%LIB%
+@set LIBRARY_PATH=%LIB%
+
+
+@set PATH=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\x86_64-w64-mingw32\lib;%PATH%
+@set PATH=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\lib;%PATH%
+
+@set PATH=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\x86_64-w64-mingw32\include;%PATH%
+@set PATH=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\include;%PATH%
+
+@set PATH=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\x86_64-w64-mingw32\bin;%PATH%
+@set PATH=E:\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;%PATH%
+@set PATH=E:\seabolt\bin;%PATH%
+@set PATH=E:\Program Files (x86)\pkg-config\bin;%PATH%
+
+@echo on
