317b98c01e34af3150f3353eeedfe09b1fe91708
AoiHosizora <a970335605@hotmail.com>
2020-12-20 14:05:32 +0800

    Update cmdrc.bat

--- bat/cmdrc.bat
+++ bat/cmdrc.bat
@@ -1,5 +1,13 @@
 @echo off
+
 doskey ll=ls -alF $*
 doskey la=ls -A $*
 doskey l=ls -CF $*
+
 doskey go2go=go2 tool go2go $*
+
+set ~=%USERPROFILE%
+doskey cd~=cd "%USERPROFILE%\$*"
+doskey cdd=cd "%USERPROFILE%\Desktop$*"
+doskey ~=cd "%USERPROFILE%\$*"
+doskey ~d=cd "%USERPROFILE%\Desktop\$*"
